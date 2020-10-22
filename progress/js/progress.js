6d0a8fc526486ec222c4b3d6dff0b92367ba1e80
30ktComputer <xuqighub@163.com>
2020-10-22 15:58:24 +0800

    add circle progress

--- progress/js/progress.js
+++ progress/js/progress.js
@@ -35,7 +35,32 @@
         }
         //将进度条置为失败状态
         failed = ()=>{
-            this.progressEl.classList.add('xui-failed-status');
+            if(this.type === 'line'){
+                this.progressEl.classList.add('xui-failed-status');
+            }else if(this.type === 'circle'){
+                this.progressEl.querySelector('.xui-progress-inner').style.setProperty('--color','#d9534f');
+                this.progressEl.querySelector('.xui-progress-text').title = 'failed';
+                this.progressEl.querySelector('.xui-progress-text').innerHTML = `
+                    <svg style="color:#d9534f;" viewBox="64 64 896 896" focusable="false" width="1em" height="1em" fill="currentColor" aria-hidden="true">
+                        <path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 00203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z"></path>
+                    </svg>
+                `;
+            }
+        }
+        //将进度条置为成功状态
+        success = ()=>{
+            //成功状态
+            if(this.type === 'line'){
+                this.progressEl.classList.add('xui-success-status');
+            }else if(this.type === 'circle'){
+                this.progressEl.querySelector('.xui-progress-inner').style.setProperty('--color','#52c41a');
+                this.progressEl.querySelector('.xui-progress-text').title = 'success';
+                this.progressEl.querySelector('.xui-progress-text').innerHTML = `
+                    <svg style="color:#52c41a;" viewBox="64 64 896 896" focusable="false"  width="1em" height="1em" fill="currentColor" aria-hidden="true">
+                        <path d="M912 190h-69.9c-9.8 0-19.1 4.5-25.1 12.2L404.7 724.5 207 474a32 32 0 00-25.1-12.2H112c-6.7 0-10.4 7.7-6.3 12.9l273.9 347c12.8 16.2 37.4 16.2 50.3 0l488.4-618.9c4.1-5.1.4-12.8-6.3-12.8z"></path>
+                    </svg>
+                `;
+            }
         }
         //更新进度条的进度，传入一个数值
         update = (percent)=>{
@@ -61,6 +86,8 @@
                     </div>
                 `);
             }else if(this.type === 'circle'){
+                //过滤标签正则
+                var re = /<[^<>]+>/g;
                 return (`
                     <div class="xui-progress-circle" ${this.width ? `style="width: ${this.width}px; height:${this.width}px;"` : ''}>
                         <div class="xui-progress-inner" style="--color:${this.color ? this.color : '#1890ff'}">
@@ -72,7 +99,7 @@
                                     style="stroke-dasharray:${Math.round(this.percent*295.31)/100}px, 295.31px; stroke-dashoffset: 0px; transition: stroke-dashoffset 0.3s ease 0s, stroke-dasharray 0.3s ease 0s, stroke 0.3s ease 0s, stroke-width 0.06s ease 0.3s;">
                                 </path>
                             </svg>
-                            <span class="xui-progress-text">${textContent}</span>
+                            <span class="xui-progress-text" title="${textContent.replace(re, '')}">${textContent}</span>
                         </div>
                     </div>
                 `);
